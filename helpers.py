d63bfbbfda01fb35570796e0e21cc9296a3a9229
Mohammad Ghorbani <mohammad.ghorbani@hotmail.com>
2023-08-21 17:33:06 +0330

    feat: adds more prints

--- helpers.py
+++ helpers.py
@@ -33,8 +33,13 @@ class TestCSVFiles(unittest.TestCase):
 
             print(f"length of input: {len(input_data)}")
             print(f"length of expected: {len(expected_content)}")
+            print()
+            for index, item in enumerate(expected_content):
+                print()
+                print(index)
+                print(item)
 
-
+            print()
             parsed_content = []
             expected_content = expected_content[1:]
 
